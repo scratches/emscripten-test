ad6d07bf206ba5a7fdd73c09978b0fff5a989ba4
Dave Syer <dsyer@vmware.com>
2022-08-26 08:58:20 +0000

    Use docker compose so volume cache works

--- shell.nix
+++ shell.nix
@@ -1,13 +1,24 @@
-with import <nixpkgs> { };
+with import <nixpkgs> {
+  overlays = [
+    (self: super: {
+      # https://github.com/NixOS/nixpkgs/pull/188162
+      binaryen = super.binaryen.override { nodejs = super.nodejs-14_x; };
+    })
+  ];
+};
 mkShell {
 
-  name = "env";
-  buildInputs = [
-    (emscripten.override { binaryen = (binaryen.override { nodejs = nodejs-14_x; }); })
+  buildInputs = with pkgs; [
+    wasmtime wabt emscripten nodejs cmake check protobuf protobufc pkg-config jbang
   ];
 
   shellHook = ''
-    echo ":emscripten:"
+    mkdir -p ~/.emscripten
+    cp -rf ${emscripten}/share/emscripten/cache ~/.emscripten
+    chmod +w -R ~/.emscripten
+    export EM_CACHE=~/.emscripten/cache
+    export TMP=/tmp
+    export TMPDIR=/tmp
   '';
 
 }
